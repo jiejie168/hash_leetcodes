9386c2a692066dd2f5df6388b95ac57788d06a1b
jiejie168 <41291332+jiejie168@users.noreply.github.com>
2020-04-24 13:55:32 +0200

    2104

--- BinaryTreeInorderTraversal.py
+++ BinaryTreeInorderTraversal.py
@@ -15,11 +15,11 @@ Output: [1,3,2]
 Follow up: Recursive solution is trivial, could you do it iteratively?
 """
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def inorderTraversal(self, root):
@@ -35,11 +35,53 @@ class Solution:
             if root.right:
                 self.helper(root.right,res)
     def inorderTraversal_2(self,root):
-        # use the iterative algorithm
+        # use the iterative algorithm, Morris Traversal
+        res=[]
+        curr=root
+        while curr !=None:
+            if curr.left==None:
+                # directly output the val of current tree node
+                # and then turn to the right node of current
+                res.append(curr.val)
+                curr=curr.right
+            else:
+                # the current has left subtree
+                pre=curr.left
+                while pre.right!=None and pre.right!=curr:
+                    # find the rightmost node
+                    pre=pre.right
+                pre.right=curr   # the curr node becomes the right node of pre.
+                tmp=curr  # store the current node
+                curr=curr.left # update the current point, which is the left node of current.
+                tmp.left=None  #set the original current left to null, avoid infinite loop
+        return res
 
+    def inorderTraversal_3(self,root):
+        # use iterative and stack.
+        curr=root
+        stacks=[] # initialize a stack
+        res=[]
+        while True:
+            # keep search
+            if curr !=None:
+                # put the current to the stack, and  search the left until None
+                stacks.append(curr)
+                curr=curr.left
+            elif stacks:
+                # pop the element in stack, and print.
+                curr=stacks.pop()
+                res.append(curr.val)
+                curr=curr.right # second search the right
+            else:
+                break
+        return res
 
+solution=Solution()
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
 
-# solution=Solution()
-# root=[1,"null",2,3]
-# ans=solution.inorderTraversal(root)
-# print (ans)
+ans=solution.inorderTraversal_3(root)
+print (ans)
---
+++ SingleNumber.py
@@ -0,0 +1,26 @@
+__author__ = 'Jie'
+"""
+136. Single Number
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+Input: [2,2,1]
+Output: 1
+
+Example 2:
+Input: [4,1,2,1,2]
+Output: 4
+
+"""
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_tab=collections.defaultdict(int)
+        for elem in nums:
+            hash_tab[elem]+=1
+        for elem in hash_tab:
+            if hash_tab[elem]==1:
+                return elem
